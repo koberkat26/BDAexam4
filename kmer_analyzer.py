3b4f203f61403ab44efd64972ce599b42b069205
Rkober26 <reese.kober@uri.edu>
2026-05-02 08:17:42 -0400

    Update tests kmer_analyzer.py

--- kmer_analyzer.py
+++ kmer_analyzer.py
@@ -1,85 +1,12 @@
-#Exam 4 BIO539
-
-import sys
-
-#creates a function to validate the seq using input values of a certain seq and the length of a substring 'k'
-#if the length of the seq is less than k, then stops= false
-#if a nucleotide in the seq             , then stops= false
-#if passes these = true
 def validate_sequence(sequence, k):
     if len(sequence) < k:
         return False
     for nucleotide in sequence:
-        if nucleotide in '1234567890':
+        if nucleotide.isdigit():
             return False
     return True
-#test for validate seq function
-def test_val_seq()
-    Obs = validate_sequence(ATGTCTGTCTGAA, 2)
-    exp = True
-    Assert exp ==obs
 
-
-#creates function to update the count of substrings
-#input values are the kmer data, a substring, and the next character
-#if a substring/kmer isnt in the data
-def update_kmer_count(kmer_data, kmer, next_char):
-    if kmer not in kmer_data:
-        kmer_data[kmer] = {'count': 1, 'next_chars': {}}
-    
-    kmer_data[kmer]['count'] += 1
-    
-    if next_char not in kmer_data[kmer]['next_chars']:
-        kmer_data[kmer]['next_chars'][next_char] = 0
-    kmer_data[kmer]['next_chars'][next_char] += 1
-
-    return kmer_data
-
-def count_kmers_with_context(sequence, k):
-    kmer_data = {}
-    
-    for i in range(len(sequence) - k):
-        kmer = sequence[i:i+k]
-        next_char = sequence[i+k]
-        
-        kmer_data = update_kmer_count(kmer_data, kmer, next_char)
-    
-    return kmer_data
-
-
-def write_results_to_file(kmer_data, output_filename):
-    sorted_kmers = sorted(kmer_data.keys())
-    
-    with open(output_filename, 'w') as f:
-        for kmer in sorted_kmers:
-            next_chars = kmer_data[kmer]['next_chars']
-            
-            next_char_str = " ".join(
-                f"{char}:{freq}" 
-                for char, freq in sorted(next_chars.items())
-            )
-            
-            f.write(f"{kmer} {next_char_str}\n")
-
-
-def main():
-    sequence_file = sys.argv[1]
-    k = int(sys.argv[2])
-    output_file = sys.argv[3]
-    
-    print(f"Reading sequences from {sequence_file}...")
-
-    with open(sequence_file, 'r') as f:
-        for sequence in f:
-            sequence = sequence.strip()
-
-            if not validate_sequence(sequence, k):
-                print(f"  Warning: Skipping sequence")
-                continue
-            
-            kmer_data = count_kmers_with_context(sequence, k) 
-            
-            write_results_to_file(kmer_data, output_file)
-
-if __name__ == '__main__':
-    main()
+#tests
+print(validate_sequence("ATCG", 3))   # should print True
+print(validate_sequence("AT1G", 3))   # should print False
+print(validate_sequence("ATG", 4))    #should print False
