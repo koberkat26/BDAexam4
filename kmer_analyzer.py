9c73504488ce4094b79f74b7b88b41389b5f0291
Rkober26 <reese.kober@uri.edu>
2026-05-02 08:39:48 -0400

    Update tests3 kmer_analyzer.py

--- kmer_analyzer.py
+++ kmer_analyzer.py
@@ -10,3 +10,29 @@ def validate_sequence(sequence, k):
 print(validate_sequence("ATCG", 3))   # should print True
 print(validate_sequence("AT1G", 3))   # should print False
 print(validate_sequence("ATG", 4))    #should print False
+
+
+def update_kmer_count(kmer_data, kmer, next_char):
+    if kmer not in kmer_data:
+        kmer_data[kmer] = {'count': 0, 'next_chars': {}}
+
+    kmer_data[kmer]['count'] += 1
+
+    if next_char not in kmer_data[kmer]['next_chars']:
+        kmer_data[kmer]['next_chars'][next_char] = 0
+
+    kmer_data[kmer]['next_chars'][next_char] += 1
+
+    return kmer_data
+#test
+if __name__ == "__main__":
+    kmer_data = {}
+
+    # simulate a DNA sequence split into k-mers and next chars
+    kmer_data = update_kmer_count(kmer_data, "ATG", "C")
+    kmer_data = update_kmer_count(kmer_data, "ATG", "C")
+    kmer_data = update_kmer_count(kmer_data, "ATG", "A")
+    kmer_data = update_kmer_count(kmer_data, "TGC", "A")
+
+    print("Final k-mer data:")
+    print(kmer_data)
